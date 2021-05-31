319b8a93db2c0af057a30405010fa631d57a7e96
Ryan Yip <ryanyip24@gmail.com>
2021-05-31 11:09:07 -0400

    Initial commit

---
+++ hiv.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import spacy
+from textpipeliner import PipelineEngine, Context
+from textpipeliner.pipes import *
+
+abstract = "HIV-1 integrates into the genome of target cells and establishes latency indefinitely. Understanding the molecular mechanism of HIV-1 latency maintenance is needed for therapeutic strategies to combat existing infection. In this study, we found an unexpected role for Apobec3A (apolipoprotein B MRNA editing enzyme catalytic subunit 3A, abbreviated \"A3A\") in maintaining the latency state within HIV-1-infected cells. Overexpression of A3A in latently infected cell lines led to lower reactivation, while knockdown or knockout of A3A led to increased spontaneous and inducible HIV-1 reactivation. A3A maintains HIV-1 latency by associating with proviral DNA at the 5' long terminal repeat region, recruiting KAP1 and HP1, and imposing repressive histone marks. We show that knockdown of A3A in latently infected human primary CD4 T cells enhanced HIV-1 reactivation. Collectively, we provide evidence and a mechanism by which A3A reinforces HIV-1 latency in infected CD4 T cells."
+
+nlp = spacy.load("en_core_web_trf")
+doc = nlp(abstract)
+
+pipes_structure = [
+    AnyPipe([
+        AggregatePipe([
+            FindTokensPipe("VERB/nsubj/*"),
+            FindTokensPipe("VERB/nsubj/*/**/*/pobj/*"),
+        ]),
+        AggregatePipe([
+            FindTokensPipe("VERB/nsubjpass/*/**/*/pobj/*/compound/*"),
+            FindTokensPipe("VERB/nsubjpass/*/**/*/pobj/*"),
+        ]),
+    ]),
+    AggregatePipe([
+        FindTokensPipe("VERB"),
+        FindTokensPipe("VERB/**/VERB"),
+    ]),
+    AggregatePipe([
+        FindTokensPipe("VERB/[dobj,pobj]/[NN,NNP,NNS]"),
+        FindTokensPipe("VERB/**/*/[dobj,pobj]/*/compound/[NN,NNP,NNS]"),
+        FindTokensPipe("VERB/**/*/[dobj,pobj]/[NN,NNP,NNS]"),
+    ])
+]
+
+engine = PipelineEngine(pipes_structure, Context(doc))
+for i in engine.process():
+    print("{}\n".format(i))
